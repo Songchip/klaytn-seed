d9c629cc4feb16eea34884f4e37c740e4d53a898
songchip <scv487@naver.com>
2019-09-06 11:20:58 +0900

    no message

--- migrations/2_deploy_card.js
+++ migrations/2_deploy_card.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const ERC721Card = artifacts.require('ERC721Card');
+const ERC721Card = artifacts.require('ERC721Card');  // 계약 정보(artifact)를 읽어와야
 
 module.exports = function(deployer) {
 	deployer.deploy(ERC721Card).then(() => {
